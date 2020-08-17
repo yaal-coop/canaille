5957029860a61e7ff7b402da2d2d7274ab510781
Éloi Rivard <eloi@yaal.fr>
2020-08-17 11:05:01 +0200

    Configuration file

--- website/app.py
+++ website/app.py
@@ -1,8 +1,8 @@
 import ldap
 import os
+import toml
 from flask import Flask, g
 
-# from .models import db
 from .oauth2 import config_oauth
 from .routes import bp
 
@@ -10,19 +10,10 @@ from .routes import bp
 def create_app(config=None):
     app = Flask(__name__)
 
-    # load default configuration
-    app.config.from_object("website.settings")
+    config = toml.load(os.environ.get("CONFIG", "config.toml"))
+    app.config.from_mapping(config)
 
-    # load environment configuration
-    if "WEBSITE_CONF" in os.environ:
-        app.config.from_envvar("WEBSITE_CONF")
-
-    # load app specified configuration
-    if config is not None:
-        if isinstance(config, dict):
-            app.config.update(config)
-        elif config.endswith(".py"):
-            app.config.from_pyfile(config)
+    app.url_map.strict_slashes = False
 
     setup_app(app)
     return app
@@ -31,8 +22,8 @@ def create_app(config=None):
 def setup_app(app):
     @app.before_request
     def before_request():
-        g.ldap = ldap.initialize("ldap://ldap")
-        g.ldap.simple_bind_s("cn=admin,dc=mydomain,dc=tld", "admin")
+        g.ldap = ldap.initialize(app.config["LDAP"]["URI"])
+        g.ldap.simple_bind_s(app.config["LDAP"]["BIND_USER"], app.config["LDAP"]["BIND_PW"])
 
     @app.after_request
     def after_request(response):
@@ -40,11 +31,5 @@ def setup_app(app):
             g.ldap.unbind_s()
         return response
 
-    #    # Create tables if they do not exist already
-    #    @app.before_first_request
-    #    def create_tables():
-    #        db.create_all()
-    #
-    #    db.init_app(app)
     config_oauth(app)
     app.register_blueprint(bp, url_prefix="")
