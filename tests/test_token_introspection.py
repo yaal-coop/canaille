b3abe210a591b9b18c805b8375ad5300a2558a90
Éloi Rivard <eloi@yaal.fr>
2020-08-24 14:44:41 +0200

    Token introspection testfile

---
+++ tests/test_token_introspection.py
@@ -0,0 +1,22 @@
+from . import client_credentials
+
+
+def test_token_introspection(testclient, user, client, token, slapd_connection):
+    res = testclient.post(
+        "/oauth/introspect",
+        params=dict(token=token.oauthAccessToken,),
+        headers={"Authorization": f"Basic {client_credentials(client)}"},
+    )
+    assert 200 == res.status_code
+    assert {
+        "active": True,
+        "client_id": token.oauthClientID,
+        "token_type": token.oauthTokenType,
+        "username": user.name,
+        "scope": token.get_scope(),
+        "sub": token.oauthSubject,
+        "aud": token.oauthClientID,
+        "iss": "https://mydomain.tld",
+        "exp": token.get_expires_at(),
+        "iat": token.get_issued_at(),
+    } == res.json
