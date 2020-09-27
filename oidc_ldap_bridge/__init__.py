d0c2a07d0f51552987561ca00bf4c10571e02a79
Éloi Rivard <eloi.rivard@aquilenet.fr>
2020-09-27 17:32:23 +0200

    Initialization improvement

--- oidc_ldap_bridge/__init__.py
+++ oidc_ldap_bridge/__init__.py
@@ -139,58 +139,59 @@ def setup_app(app):
         )
 
         babel = Babel(app)
+
+        @babel.localeselector
+        def get_locale():
+            user = getattr(g, "user", None)
+            if user is not None:
+                return user.locale
+
+            if app.config.get("LANGUAGE"):
+                return app.config.get("LANGUAGE")
+
+            return request.accept_languages.best_match(["fr", "en"])
+
+        @babel.timezoneselector
+        def get_timezone():
+            user = getattr(g, "user", None)
+            if user is not None:
+                return user.timezone
+
+        @app.before_request
+        def before_request():
+            g.ldap = ldap.initialize(app.config["LDAP"]["URI"])
+            g.ldap.simple_bind_s(
+                app.config["LDAP"]["BIND_DN"], app.config["LDAP"]["BIND_PW"]
+            )
+
+        @app.after_request
+        def after_request(response):
+            if "ldap" in g:
+                g.ldap.unbind_s()
+            return response
+
+        @app.context_processor
+        def global_processor():
+            return {
+                "logo_url": app.config.get("LOGO"),
+                "website_name": app.config.get("NAME"),
+                "user": current_user(),
+                "menu": True,
+            }
+
+        @app.errorhandler(403)
+        def unauthorized(e):
+            return render_template("error.html", error=403), 403
+
+        @app.errorhandler(404)
+        def page_not_found(e):
+            return render_template("error.html", error=404), 404
+
+        @app.errorhandler(500)
+        def server_error(e):
+            return render_template("error.html", error=500), 500
+
     except Exception as exc:
         if SENTRY and app.config.get("SENTRY_DSN"):
             raise
         sentry_sdk.capture_exception(exc)
-
-    @app.before_request
-    def before_request():
-        g.ldap = ldap.initialize(app.config["LDAP"]["URI"])
-        g.ldap.simple_bind_s(
-            app.config["LDAP"]["BIND_DN"], app.config["LDAP"]["BIND_PW"]
-        )
-
-    @app.after_request
-    def after_request(response):
-        if "ldap" in g:
-            g.ldap.unbind_s()
-        return response
-
-    @app.context_processor
-    def global_processor():
-        return {
-            "logo_url": app.config.get("LOGO"),
-            "website_name": app.config.get("NAME"),
-            "user": current_user(),
-            "menu": True,
-        }
-
-    @babel.localeselector
-    def get_locale():
-        user = getattr(g, "user", None)
-        if user is not None:
-            return user.locale
-
-        if app.config.get("LANGUAGE"):
-            return app.config.get("LANGUAGE")
-
-        return request.accept_languages.best_match(["fr", "en"])
-
-    @babel.timezoneselector
-    def get_timezone():
-        user = getattr(g, "user", None)
-        if user is not None:
-            return user.timezone
-
-    @app.errorhandler(403)
-    def unauthorized(e):
-        return render_template("error.html", error=403), 403
-
-    @app.errorhandler(404)
-    def page_not_found(e):
-        return render_template("error.html", error=404), 404
-
-    @app.errorhandler(500)
-    def server_error(e):
-        return render_template("error.html", error=500), 500
