962d3d34f6a8ec9b5f12b0f810a7987b61ed58b9
Éloi Rivard <eloi.rivard@aquilenet.fr>
2020-10-23 12:20:27 +0200

    Minor bugfix

--- canaille/commands.py
+++ canaille/commands.py
@@ -7,7 +7,7 @@ bp = Blueprint("commands", __name__)
 
 @bp.cli.command("clean")
 def clean():
-    from canaille import setup_ldap
+    from canaille import setup_ldap, teardown_ldap
 
     setup_ldap(current_app)
 
