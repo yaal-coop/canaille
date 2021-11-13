1586bb5a55f53d945bcd4d015e391366185f30a9
Éloi Rivard <eloi.rivard@aquilenet.fr>
2021-11-13 19:11:56 +0100

    install command creates jwt keypair

--- tests/commands/test_install.py
+++ tests/commands/test_install.py
@@ -1,4 +1,5 @@
 import pytest
+import os
 from canaille.commands import cli
 from canaille import create_app
 from flask_webtest import TestApp
@@ -46,9 +47,26 @@ def slapd_server():
 def test_setup_ldap_tree(slapd_server, configuration):
     output = slapd_server.slapcat().stdout.decode("utf-8")
     assert "dn: ou=tokens,ou=oauth,dc=slapd-test,dc=python-ldap,dc=org" not in output
-    testclient = TestApp(create_app(configuration))
+    testclient = TestApp(create_app(configuration, validate=False))
     runner = testclient.app.test_cli_runner()
     runner.invoke(cli, ["install"])
 
     output = slapd_server.slapcat().stdout.decode("utf-8")
     assert "dn: ou=tokens,ou=oauth,dc=slapd-test,dc=python-ldap,dc=org" in output
+
+
+def test_install_keypair(configuration, tmpdir):
+    keys_dir = os.path.join(tmpdir, "keys")
+    os.makedirs(keys_dir)
+    configuration["JWT"]["PRIVATE_KEY"] = os.path.join(keys_dir, "private.pem")
+    configuration["JWT"]["PUBLIC_KEY"] = os.path.join(keys_dir, "public.pem")
+
+    assert not os.path.exists(configuration["JWT"]["PRIVATE_KEY"])
+    assert not os.path.exists(configuration["JWT"]["PUBLIC_KEY"])
+
+    testclient = TestApp(create_app(configuration, validate=False))
+    runner = testclient.app.test_cli_runner()
+    runner.invoke(cli, ["install"])
+
+    assert os.path.exists(configuration["JWT"]["PRIVATE_KEY"])
+    assert os.path.exists(configuration["JWT"]["PUBLIC_KEY"])
