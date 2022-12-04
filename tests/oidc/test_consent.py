a4afcc61dda9ce87bbd8ba020d3af39946e782f2
Éloi Rivard <eloi.rivard@nubla.fr>
2022-12-04 13:57:56 +0100

    unit tests: added consent deletion tests

--- tests/oidc/test_consent.py
+++ tests/oidc/test_consent.py
@@ -2,15 +2,29 @@ def test_no_logged_no_access(testclient):
     testclient.get("/consent", status=403)
 
 
-def test_consent_list(testclient, client, consent, logged_user, token):
+def test_delete(testclient, client, consent, logged_user, token):
     res = testclient.get("/consent", status=200)
     assert client.client_name in res.text
     assert not token.revoked
 
     res = testclient.get(f"/consent/delete/{consent.cn[0]}", status=302)
-
     res = res.follow(status=200)
+    assert "The access has been revoked" in res.text
     assert client.client_name not in res.text
 
     token.reload()
     assert token.revoked
+
+
+def test_invalid_consent_delete(testclient, client, logged_user):
+    res = testclient.get(f"/consent/delete/invalid", status=302)
+    res = res.follow(status=200)
+    assert "The access has been revoked" not in res.text
+    assert "Could not delete this access" in res.text
+
+
+def test_someone_else_consent_delete(testclient, client, consent, logged_moderator):
+    res = testclient.get(f"/consent/delete/{consent.cn[0]}", status=302)
+    res = res.follow(status=200)
+    assert "The access has been revoked" not in res.text
+    assert "Could not delete this access" in res.text
