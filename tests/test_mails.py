8b066c7695ffb7232931a2d55e823cd09894f076
Éloi Rivard <eloi.rivard@nubla.fr>
2022-12-17 00:38:05 +0100

    unit tests: test mails domain guessing from SERVER_NAME flask configuration

--- tests/test_mails.py
+++ tests/test_mails.py
@@ -1,6 +1,10 @@
 import warnings
 from unittest import mock
 
+import ldap
+from flask import g
+from flask_webtest import TestApp
+
 
 def test_send_test_email(testclient, logged_admin, smtpd):
     assert len(smtpd.messages) == 0
@@ -45,3 +49,22 @@ def test_default_from_addr(testclient, user, smtpd):
     res = res.form.submit(status=200)
     assert smtpd.messages[0]["X-MailFrom"] == "admin@localhost"
     assert smtpd.messages[0]["From"] == '"Canaille" <admin@localhost>'
+
+
+def test_default_from_flask_server_name(app, user, smtpd, slapd_server):
+    del app.config["SMTP"]["FROM_ADDR"]
+    app.config["SERVER_NAME"] = "foobar.tld"
+
+    with app.app_context():
+        g.ldap = ldap.ldapobject.SimpleLDAPObject(slapd_server.ldap_uri)
+        g.ldap.protocol_version = 3
+        g.ldap.simple_bind_s(slapd_server.root_dn, slapd_server.root_pw)
+
+        testclient = TestApp(app)
+        res = testclient.get("/reset", status=200)
+        res.form["login"] = "user"
+        res = res.form.submit(status=200)
+        assert smtpd.messages[0]["X-MailFrom"] == "admin@foobar.tld"
+        assert smtpd.messages[0]["From"] == '"Canaille" <admin@foobar.tld>'
+
+        g.ldap.unbind_s()
