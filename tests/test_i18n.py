c1b3a6475791e8fae889e16096972bb62db61337
Éloi Rivard <eloi.rivard@nubla.fr>
2023-01-29 23:06:12 +0100

    Use pyquery in i18n tests

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -7,36 +7,36 @@ def test_preferred_language(testclient, logged_user):
 
     res = testclient.get("/profile/user", status=200)
     assert res.form["preferredLanguage"].value == "auto"
+    assert res.pyquery("html")[0].attrib["lang"] == "en"
     assert "My profile" in res.text
     assert "Mon profil" not in res.text
-    assert '<html lang="en">' in res.text
 
     res.form["preferredLanguage"] = "fr"
     res = res.form.submit(name="action", value="edit").follow()
     logged_user = User.get(dn=logged_user.dn)
     assert logged_user.preferredLanguage == "fr"
     assert res.form["preferredLanguage"].value == "fr"
+    assert res.pyquery("html")[0].attrib["lang"] == "fr"
     assert "My profile" not in res.text
     assert "Mon profil" in res.text
-    assert '<html lang="fr">' in res.text
 
     res.form["preferredLanguage"] = "en"
     res = res.form.submit(name="action", value="edit").follow()
     logged_user = User.get(dn=logged_user.dn)
     assert logged_user.preferredLanguage == "en"
     assert res.form["preferredLanguage"].value == "en"
+    assert res.pyquery("html")[0].attrib["lang"] == "en"
     assert "My profile" in res.text
     assert "Mon profil" not in res.text
-    assert '<html lang="en">' in res.text
 
     res.form["preferredLanguage"] = "auto"
     res = res.form.submit(name="action", value="edit").follow()
     logged_user = User.get(dn=logged_user.dn)
     assert logged_user.preferredLanguage is None
     assert res.form["preferredLanguage"].value == "auto"
+    assert res.pyquery("html")[0].attrib["lang"] == "en"
     assert "My profile" in res.text
     assert "Mon profil" not in res.text
-    assert '<html lang="en">' in res.text
 
 
 def test_language_config(testclient, logged_user):
@@ -44,12 +44,12 @@ def test_language_config(testclient, logged_user):
     logged_user.save()
 
     res = testclient.get("/profile/user", status=200)
+    assert res.pyquery("html")[0].attrib["lang"] == "en"
     assert "My profile" in res.text
     assert "Mon profil" not in res.text
-    assert '<html lang="en">' in res.text
 
     testclient.app.config["LANGUAGE"] = "fr"
     res = testclient.get("/profile/user", status=200)
+    assert res.pyquery("html")[0].attrib["lang"] == "fr"
     assert "My profile" not in res.text
     assert "Mon profil" in res.text
-    assert '<html lang="fr">' in res.text
