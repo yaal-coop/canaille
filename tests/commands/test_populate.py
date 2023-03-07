a368b36d9c193b669daaaa97a6848c4b1be916ab
Éloi Rivard <eloi.rivard@nubla.fr>
2023-03-07 17:27:31 +0100

    Better populate test cleaning

--- tests/commands/test_populate.py
+++ tests/commands/test_populate.py
@@ -1,6 +1,7 @@
 from canaille.commands import cli
 from canaille.models import Group
 from canaille.models import User
+from canaille.populate import fake_users
 
 
 def test_populate_users(testclient, slapd_connection):
@@ -8,14 +9,23 @@ def test_populate_users(testclient, slapd_connection):
 
     assert len(User.all()) == 0
     res = runner.invoke(cli, ["populate", "--nb", "10", "users"])
-    assert res.exit_code == 0, res.stderr
+    assert res.exit_code == 0, res.stdout
     assert len(User.all()) == 10
+    for user in User.all():
+        user.delete()
 
 
 def test_populate_groups(testclient, slapd_connection):
+    fake_users(10)
     runner = testclient.app.test_cli_runner()
 
     assert len(Group.all()) == 0
     res = runner.invoke(cli, ["populate", "--nb", "10", "groups"])
-    assert res.exit_code == 0, res.stderr
+    assert res.exit_code == 0, res.stdout
     assert len(Group.all()) == 10
+
+    for group in Group.all():
+        group.delete()
+
+    for user in User.all():
+        user.delete()
