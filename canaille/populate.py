705150942ea4182efcc011bd5addd76e0fc3c1b3
Éloi Rivard <eloi.rivard@nubla.fr>
2023-03-12 17:42:32 +0100

    Populate ignore unicity errors if launched several times in a row

--- canaille/populate.py
+++ canaille/populate.py
@@ -1,6 +1,7 @@
 import random
 
 import faker
+import ldap
 from canaille.i18n import available_language_codes
 from canaille.models import Group
 from canaille.models import User
@@ -16,29 +17,32 @@ def fake_users(nb=1):
     fakes = faker_generator()
     users = list()
     for _ in range(nb):
-        fake = random.choice(fakes)
-        name = fake.unique.name()
-        user = User(
-            cn=name,
-            givenName=name.split(" ")[0],
-            sn=name.split(" ")[1],
-            uid=fake.unique.user_name(),
-            mail=fake.unique.email(),
-            telephoneNumber=fake.unique.ssn(),
-            labeledURI=fake.unique.uri(),
-            postalAddress=fake.unique.address(),
-            street=fake.street_name(),
-            postalCode=fake.postcode(),
-            l=fake.city(),
-            st=fake.state(),
-            employeeNumber=str(fake.unique.random_number()),
-            departmentNumber=fake.word(),
-            title=fake.job(),
-            userPassword=fake.password(),
-            preferredLanguage=fake._locales[0],
-        )
-        user.save()
-        users.append(user)
+        try:
+            fake = random.choice(fakes)
+            name = fake.unique.name()
+            user = User(
+                cn=name,
+                givenName=name.split(" ")[0],
+                sn=name.split(" ")[1],
+                uid=fake.unique.user_name(),
+                mail=fake.unique.email(),
+                telephoneNumber=fake.unique.ssn(),
+                labeledURI=fake.unique.uri(),
+                postalAddress=fake.unique.address(),
+                street=fake.street_name(),
+                postalCode=fake.postcode(),
+                l=fake.city(),
+                st=fake.state(),
+                employeeNumber=str(fake.unique.random_number()),
+                departmentNumber=fake.word(),
+                title=fake.job(),
+                userPassword=fake.password(),
+                preferredLanguage=fake._locales[0],
+            )
+            user.save()
+            users.append(user)
+        except ldap.ALREADY_EXISTS:  # pragma: no cover
+            pass
     return users
 
 
@@ -47,12 +51,15 @@ def fake_groups(nb=1, nb_users_max=1):
     users = User.query()
     groups = list()
     for _ in range(nb):
-        group = Group(
-            cn=fake.unique.word(),
-            description=fake.sentence(),
-        )
-        nb_users = random.randrange(1, nb_users_max + 1)
-        group.member = list({random.choice(users) for _ in range(nb_users)})
-        group.save()
-        groups.append(group)
+        try:
+            group = Group(
+                cn=fake.unique.word(),
+                description=fake.sentence(),
+            )
+            nb_users = random.randrange(1, nb_users_max + 1)
+            group.member = list({random.choice(users) for _ in range(nb_users)})
+            group.save()
+            groups.append(group)
+        except ldap.ALREADY_EXISTS:  # pragma: no cover
+            pass
     return groups
