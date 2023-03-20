6729cc91135aad23886df7f382d3f69a47aa43ab
Éloi Rivard <eloi.rivard@nubla.fr>
2023-03-20 09:56:06 +0100

    Fixed demo/run.sh exit commands

--- demo/run.sh
+++ demo/run.sh
@@ -4,20 +4,20 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 if ! type slapd > /dev/null 2>&1; then
     echo "Cannot start the LDAP server. Please install OpenLDAP on your system"
     echo "or run the demo with docker-compose."
-    return 1
+    exit 1
 fi
 
 if ! type python > /dev/null 2>&1 && ! type python3 > /dev/null 2>&1; then
     echo "Cannot start the LDAP server. Please install python on your system"
     echo "or run the demo with docker-compose."
-    return 1
+    exit 1
 fi
 
 if ! type poetry > /dev/null 2>&1; then
     echo "Cannot start the LDAP server. Please install poetry on your system"
     echo "or run the demo with docker-compose."
     echo "https://python-poetry.org/docs/#installation"
-    return 1
+    exit 1
 fi
 
 poetry install --with demo --without dev
