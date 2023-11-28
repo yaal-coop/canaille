410d4642426a8f7e2cf1733dacf6a45c0b98a59c
Éloi Rivard <eloi@yaal.coop>
2023-11-28 18:51:28 +0100

    refactor: force objects to be string for hashing

--- canaille/app/__init__.py
+++ canaille/app/__init__.py
@@ -18,7 +18,7 @@ def b64_to_obj(string):
 def build_hash(*args):
     return hashlib.sha256(
         current_app.config["SECRET_KEY"].encode("utf-8")
-        + obj_to_b64(args).encode("utf-8")
+        + obj_to_b64(str(args)).encode("utf-8")
     ).hexdigest()
 
 
