e09a3071d4bc40f91fcd6ffc3684ed25d7825bb1
Éloi Rivard <eloi@yaal.coop>
2023-12-01 11:49:09 +0100

    fix: handle 4xx and 5xx error codes with htmx

--- canaille/static/js/base.js
+++ canaille/static/js/base.js
@@ -12,3 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 document.addEventListener('htmx:load', onDomChanges);
+
+document.body.addEventListener('htmx:beforeOnLoad', function (evt) {
+    if (evt.detail.xhr.status >= 400) {
+        evt.detail.shouldSwap = true;
+        evt.detail.isError = false;
+    }
+});
