a21dbd52626bff556223e703f7a16d68bcd5e097
sebastien <sebastien@yaal.coop>
2024-10-28 10:10:12 +0100

    mainains only zxcvbn for password strength calculator. It will be easier to chose an other password strength calculator this way.

--- canaille/app/forms.py
+++ canaille/app/forms.py
@@ -71,41 +71,11 @@ def password_too_long_validator(form, field):
 
 
 def password_strength_calculator(password):
-    """Each type of character increases the difficulty of cracking the password.
-    This result is multiplied by itself depending on the length of the password: power length of the string.
-    Finally, a logarithm transformation allows to obtain a result very often less than 100.
-    We will divide this result by 4 to multiply it by the score given by zxcvbn (0 to 4).
-
-    We chose to combine zxcvbn with a home-made algorithm to ensure that password length has sufficient influence in measuring password strength.
-    """
-
     strength_score = 0
 
     if password and type(password) is str:
-        has_lower = any(c.islower() for c in password)
-        has_upper = any(c.isupper() for c in password)
-        has_digit = any(c.isdigit() for c in password)
-        has_special = any(not c.isalnum() for c in password)
-
-        strength_score = min(
-            100,
-            round(
-                math.log10(
-                    sum(
-                        [
-                            has_lower * 26,
-                            has_upper * 26,
-                            has_digit * 10,
-                            has_special * 33,
-                        ]
-                    )
-                    ** len(password)
-                )
-                / 4
-            ),
-        )
-
-        strength_score *= zxcvbn(password).score
+        strength_score = zxcvbn(password).score
+        strength_score = strength_score * 100 // 4
 
     return strength_score
 
