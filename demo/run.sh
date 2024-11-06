c57244b9fa3c1d04bfb51b9bb30f4c7358ec7043
Éloi Rivard <eloi@yaal.coop>
2024-11-06 15:21:13 +0100

    fix: zxcvbn compilation in demo
    until https://github.com/fief-dev/zxcvbn-rs-py/issues/2 is fixed, it is
    needed to manually force the compatibility with Python 3.13

--- demo/run.sh
+++ demo/run.sh
@@ -22,6 +22,10 @@ fi
 
 pushd "$DIR" > /dev/null 2>&1 || exit
 
+# Needed until zxcvbn supports Python 3.13
+# https://github.com/fief-dev/zxcvbn-rs-py/issues/2
+export PYO3_USE_ABI3_FORWARD_COMPATIBILITY=1
+
 uv sync --group demo --all-extras
 
 if [ "$BACKEND" = "memory" ]; then
