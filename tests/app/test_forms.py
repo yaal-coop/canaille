d951c2e18da60a48409516d8b17f15bf75227169
sebastien <sebastien@yaal.coop>
2024-11-08 15:15:12 +0100

    new tests on failure of api request with htmx and without SMTP

--- tests/app/test_forms.py
+++ tests/app/test_forms.py
@@ -1,4 +1,5 @@
 import datetime
+from unittest import mock
 
 import pytest
 import wtforms
@@ -354,3 +355,46 @@ def test_compromised_password_validator(testclient):
         compromised_password_validator(None, Field("zxcvbn123"))
     with pytest.raises(wtforms.ValidationError):
         compromised_password_validator(None, Field("azertyuiop123"))
+
+
+@mock.patch("requests.api.get")
+def test_compromised_password_validator_with_failure_of_api_request_and_no_SMTP_in_config(
+    api_get, testclient, logged_user
+):
+    api_get.side_effect = mock.Mock(side_effect=Exception())
+    current_app.config["CANAILLE"]["SMTP"] = None
+
+    class Field:
+        def __init__(self, data):
+            self.data = data
+
+    compromised_password_validator(None, Field("i'm a little pea"))
+    compromised_password_validator(None, Field("i'm a little chickpea"))
+    compromised_password_validator(None, Field("i'm singing in the rain"))
+    compromised_password_validator(None, Field("password"))
+    compromised_password_validator(None, Field("987654321"))
+    compromised_password_validator(None, Field("correct horse battery staple"))
+    compromised_password_validator(None, Field("zxcvbn123"))
+    compromised_password_validator(None, Field("azertyuiop123"))
+
+
+@mock.patch("requests.api.get")
+def test_compromised_password_validator_with_failure_of_api_request_and_only_with_htmx(
+    api_get, testclient, logged_user
+):
+    api_get.side_effect = mock.Mock(side_effect=Exception())
+
+    res = testclient.get("/profile/user/settings")
+    res = testclient.post(
+        "/profile/user/settings",
+        {
+            "csrf_token": res.form["csrf_token"].value,
+            "password1": "correct horse battery staple",
+        },
+        headers={
+            "HX-Request": "true",
+            "HX-Trigger-Name": "password1",
+        },
+    )
+
+    res.mustcontain('data-percent="100"')
