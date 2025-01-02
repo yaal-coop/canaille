e1ca7dfe1eca2d129186a14438edd48e9102f31b
Éloi Rivard <eloi@yaal.coop>
2025-01-02 23:29:58 +0100

    feat: --version CLI option

--- canaille/commands.py
+++ canaille/commands.py
@@ -1,3 +1,5 @@
+import importlib.metadata
+
 import click
 from flask.cli import FlaskGroup
 
@@ -7,6 +9,8 @@ import canaille.core.commands
 import canaille.oidc.commands
 from canaille import create_app
 
+version = importlib.metadata.version("canaille")
+
 
 @click.group(
     cls=FlaskGroup,
@@ -14,6 +18,7 @@ from canaille import create_app
     add_version_option=False,
     add_default_commands=False,
 )
+@click.version_option(version, prog_name="Canaille")
 def cli():
     """Canaille management utilities."""
 
@@ -22,3 +27,7 @@ canaille.app.commands.register(cli)
 canaille.backends.commands.register(cli)
 canaille.core.commands.register(cli)
 canaille.oidc.commands.register(cli)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    cli()
