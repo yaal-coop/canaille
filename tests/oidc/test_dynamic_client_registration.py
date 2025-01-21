01e847d4466d02b5ce2172f2bc9efcd7ceed7d4b
Brunelie Lauret <brunelie@yaal.coop>
2025-01-21 08:53:03 +0100

    adds test for client registration without a scope

--- tests/oidc/test_dynamic_client_registration.py
+++ tests/oidc/test_dynamic_client_registration.py
@@ -180,6 +180,60 @@ def test_client_registration_with_software_statement(testclient, backend, keypai
     backend.delete(client)
 
 
+def test_client_registration_with_software_statement_no_scope(
+    testclient, backend, keypair
+):
+    private_key, _ = keypair
+    testclient.app.config["CANAILLE_OIDC"]["DYNAMIC_CLIENT_REGISTRATION_OPEN"] = True
+
+    software_statement_payload = {
+        "software_id": "4NRB1-0XZABZI9E6-5SM3R",
+        "client_name": "Example Statement-based Client",
+        "client_uri": "https://client.example.test/",
+        "response_types": ["code"],
+        "grant_types": ["authorization_code"],
+    }
+    software_statement_header = {"alg": "RS256"}
+    software_statement = jwt.encode(
+        software_statement_header, software_statement_payload, private_key
+    ).decode()
+
+    payload = {
+        "redirect_uris": [
+            "https://client.example.test/callback",
+            "https://client.example.test/callback2",
+        ],
+        "software_statement": software_statement,
+        "scope": "",
+    }
+    res = testclient.post_json("/oauth/register", payload, status=201)
+
+    client = backend.get(models.Client, client_id=res.json["client_id"])
+    assert res.json == {
+        "client_id": client.client_id,
+        "client_secret": client.client_secret,
+        "client_id_issued_at": mock.ANY,
+        "client_secret_expires_at": 0,
+        "redirect_uris": [
+            "https://client.example.test/callback",
+            "https://client.example.test/callback2",
+        ],
+        "grant_types": ["authorization_code"],
+        "response_types": ["code"],
+        "scope": "",
+        "token_endpoint_auth_method": "client_secret_basic",
+        "client_name": "Example Statement-based Client",
+        "client_uri": "https://client.example.test/",
+        "software_id": "4NRB1-0XZABZI9E6-5SM3R",
+    }
+    assert client.redirect_uris == [
+        "https://client.example.test/callback",
+        "https://client.example.test/callback2",
+    ]
+    assert client.token_endpoint_auth_method == "client_secret_basic"
+    backend.delete(client)
+
+
 def test_client_registration_without_authentication_ok(testclient, backend):
     testclient.app.config["CANAILLE_OIDC"]["DYNAMIC_CLIENT_REGISTRATION_OPEN"] = True
 
