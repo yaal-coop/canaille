ca57764c97de5746a15607ec2be9f77a3c0efc53
Éloi Rivard <eloi@yaal.coop>
2025-02-13 10:39:56 +0100

    fix: OIDC well-known endpoint fixes
    also, set explicitly all values defined in the specs
    
    https://datatracker.ietf.org/doc/html/rfc8414
    https://openid.net/specs/openid-connect-discovery-1_0-errata2.html

--- canaille/oidc/oauth.py
+++ canaille/oidc/oauth.py
@@ -64,9 +64,13 @@ def oauth_authorization_server():
         "token_endpoint_auth_signing_alg_values_supported": ["RS256", "ES256"],
         "userinfo_endpoint": url_for("oidc.endpoints.userinfo", _external=True),
         "revocation_endpoint": url_for("oidc.endpoints.revoke_token", _external=True),
+        "revocation_endpoint_auth_methods_supported": ["client_secret_basic"],
+        "revocation_endpoint_auth_signing_alg_values_supported": None,
         "introspection_endpoint": url_for(
             "oidc.endpoints.introspect_token", _external=True
         ),
+        "introspection_endpoint_auth_methods_supported": None,
+        "introspection_endpoint_auth_signing_alg_values_supported": None,
         "jwks_uri": url_for("oidc.endpoints.jwks", _external=True),
         "registration_endpoint": url_for(
             "oidc.endpoints.client_registration", _external=True
@@ -87,7 +91,7 @@ def oauth_authorization_server():
             "code id_token",
             "token id_token",
         ],
-        "grant_types": [
+        "grant_types_supported": [
             "authorization_code",
             "implicit",
             "password",
@@ -98,13 +102,19 @@ def oauth_authorization_server():
         "code_challenge_methods_supported": CodeChallenge.SUPPORTED_CODE_CHALLENGE_METHOD,
         "service_documentation": DOCUMENTATION_URL,
         "authorization_response_iss_parameter_supported": True,
+        "op_policy_uri": None,
+        "op_tos_uri": None,
     }
 
 
 def openid_configuration():
+    prompt_values_supported = ["none"] + (
+        ["create"] if current_app.config["CANAILLE"]["ENABLE_REGISTRATION"] else []
+    )
     return {
         **oauth_authorization_server(),
         "end_session_endpoint": url_for("oidc.endpoints.end_session", _external=True),
+        "claim_types_supported": ["normal"],
         "claims_supported": [
             "sub",
             "iss",
@@ -124,11 +134,25 @@ def openid_configuration():
             "groups",
             "nonce",
         ],
-        "subject_types_supported": ["pairwise", "public"],
+        "claims_locales_supported": None,
+        "claims_parameter_supported": False,
+        "subject_types_supported": ["public"],
         "id_token_signing_alg_values_supported": ["RS256", "ES256", "HS256"],
+        "id_token_encryption_alg_values_supported": None,
+        "id_token_encryption_enc_values_supported": None,
         "userinfo_signing_alg_values_supported": ["RS256"],
-        "prompt_values_supported": ["none"]
-        + (["create"] if current_app.config["CANAILLE"]["ENABLE_REGISTRATION"] else []),
+        "userinfo_encryption_alg_values_supported": None,
+        "userinfo_encryption_enc_values_supported": None,
+        "prompt_values_supported": prompt_values_supported,
+        "request_parameter_supported": False,
+        "request_uri_parameter_supported": False,
+        "require_request_uri_registration": False,
+        "request_object_signing_alg_values_supported": ["none", "RS256"],
+        "request_object_encryption_alg_values_supported": None,
+        "request_object_encryption_enc_values_supported": None,
+        "response_modes_supported": ["query", "fragment"],
+        "acr_values_supported": None,
+        "display_values_supported": None,
     }
 
 
--- tests/oidc/test_well_known.py
+++ tests/oidc/test_well_known.py
@@ -7,7 +7,7 @@ def test_oauth_authorization_server(testclient):
     assert res == {
         "authorization_endpoint": "http://canaille.test/oauth/authorize",
         "code_challenge_methods_supported": ["plain", "S256"],
-        "grant_types": [
+        "grant_types_supported": [
             "authorization_code",
             "implicit",
             "password",
@@ -15,8 +15,12 @@ def test_oauth_authorization_server(testclient):
             "refresh_token",
         ],
         "introspection_endpoint": "http://canaille.test/oauth/introspect",
+        "introspection_endpoint_auth_methods_supported": None,
+        "introspection_endpoint_auth_signing_alg_values_supported": None,
         "issuer": "https://auth.test",
         "jwks_uri": "http://canaille.test/oauth/jwks.json",
+        "op_policy_uri": None,
+        "op_tos_uri": None,
         "registration_endpoint": "http://canaille.test/oauth/register",
         "response_types_supported": [
             "code",
@@ -27,6 +31,10 @@ def test_oauth_authorization_server(testclient):
             "token id_token",
         ],
         "revocation_endpoint": "http://canaille.test/oauth/revoke",
+        "revocation_endpoint_auth_methods_supported": [
+            "client_secret_basic",
+        ],
+        "revocation_endpoint_auth_signing_alg_values_supported": None,
         "scopes_supported": [
             "openid",
             "profile",
@@ -54,7 +62,13 @@ def test_openid_configuration(testclient):
     res = testclient.get("/.well-known/openid-configuration", status=200).json
     assert "https://auth.test" == res["issuer"]
     assert res == {
+        "acr_values_supported": None,
         "authorization_endpoint": "http://canaille.test/oauth/authorize",
+        "claim_types_supported": [
+            "normal",
+        ],
+        "claims_locales_supported": None,
+        "claims_parameter_supported": False,
         "claims_supported": [
             "sub",
             "iss",
@@ -75,20 +89,40 @@ def test_openid_configuration(testclient):
             "nonce",
         ],
         "code_challenge_methods_supported": ["plain", "S256"],
+        "display_values_supported": None,
         "end_session_endpoint": "http://canaille.test/oauth/end_session",
-        "grant_types": [
+        "grant_types_supported": [
             "authorization_code",
             "implicit",
             "password",
             "client_credentials",
             "refresh_token",
         ],
+        "id_token_encryption_alg_values_supported": None,
+        "id_token_encryption_enc_values_supported": None,
         "id_token_signing_alg_values_supported": ["RS256", "ES256", "HS256"],
         "userinfo_signing_alg_values_supported": ["RS256"],
         "introspection_endpoint": "http://canaille.test/oauth/introspect",
+        "introspection_endpoint_auth_methods_supported": None,
+        "introspection_endpoint_auth_signing_alg_values_supported": None,
         "issuer": "https://auth.test",
         "jwks_uri": "http://canaille.test/oauth/jwks.json",
+        "op_policy_uri": None,
+        "op_tos_uri": None,
         "registration_endpoint": "http://canaille.test/oauth/register",
+        "request_object_encryption_alg_values_supported": None,
+        "request_object_encryption_enc_values_supported": None,
+        "request_object_signing_alg_values_supported": [
+            "none",
+            "RS256",
+        ],
+        "request_parameter_supported": False,
+        "request_uri_parameter_supported": False,
+        "require_request_uri_registration": False,
+        "response_modes_supported": [
+            "query",
+            "fragment",
+        ],
         "response_types_supported": [
             "code",
             "token",
@@ -98,6 +132,10 @@ def test_openid_configuration(testclient):
             "token id_token",
         ],
         "revocation_endpoint": "http://canaille.test/oauth/revoke",
+        "revocation_endpoint_auth_methods_supported": [
+            "client_secret_basic",
+        ],
+        "revocation_endpoint_auth_signing_alg_values_supported": None,
         "scopes_supported": [
             "openid",
             "profile",
@@ -107,7 +145,7 @@ def test_openid_configuration(testclient):
             "groups",
         ],
         "service_documentation": "https://canaille.readthedocs.io",
-        "subject_types_supported": ["pairwise", "public"],
+        "subject_types_supported": ["public"],
         "token_endpoint": "http://canaille.test/oauth/token",
         "token_endpoint_auth_methods_supported": [
             "client_secret_basic",
@@ -118,6 +156,8 @@ def test_openid_configuration(testclient):
         "token_endpoint_auth_signing_alg_values_supported": ["RS256", "ES256"],
         "ui_locales_supported": g.available_language_codes,
         "userinfo_endpoint": "http://canaille.test/oauth/userinfo",
+        "userinfo_encryption_alg_values_supported": None,
+        "userinfo_encryption_enc_values_supported": None,
         "prompt_values_supported": ["none"],
         "authorization_response_iss_parameter_supported": True,
     }
