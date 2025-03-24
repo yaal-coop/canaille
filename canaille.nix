3afd7049873b83cb6a45c591796335452fcc20c4
Éloi Rivard <eloi@yaal.coop>
2025-03-24 13:15:24 +0100

    chore: remove explicit ACLs from the Nix package

--- canaille.nix
+++ canaille.nix
@@ -29,60 +29,14 @@ pkgs.dockerTools.buildImage {
         SERVER_NAME = \"canaille.localhost:5000\"\n
         PREFERRED_URL_SCHEME = \"https\"\n
         [CANAILLE]\n
-        ENABLE_REGISTRATION = 1\n
-        ADMIN_EMAIL = \"admin@example.org\"\n
         TIMEZONE = \"UTC\"\n
         [CANAILLE_SQL]\n
         DATABASE_URI = \"sqlite:///demo.sqlite\"\n
-        [CANAILLE.ACL.DEFAULT]\n
-        PERMISSIONS = [\"edit_self\", \"use_oidc\"]\n
-        READ = [
-            \"user_name\",
-            \"groups\",
-            \"lock_date\",
-        ]\n
-        WRITE = [
-            \"photo\",
-            \"given_name\",
-            \"family_name\",
-            \"display_name\",
-            \"password\",
-            \"phone_numbers\",
-            \"emails\",
-            \"profile_url\",
-            \"formatted_address\",
-            \"street\",
-            \"postal_code\",
-            \"locality\",
-            \"region\",
-            \"preferred_language\",
-            \"employee_number\",
-            \"department\",
-            \"title\",
-            \"organization\",
-        ]\n
-        [CANAILLE.ACL.ADMIN]\n
-        FILTER = {groups = \"admins\"}\n
-        PERMISSIONS = [
-            \"manage_users\",
-            \"manage_groups\",
-            \"manage_oidc\",
-            \"delete_account\",
-            \"impersonate_users\",
-        ]\n
-        WRITE = [
-            \"groups\",
-            \"lock_date\",
-        ]\n
-        [CANAILLE.ACL.HALF_ADMIN]\n
-        FILTER = {groups = \"moderators\"}\n
-        PERMISSIONS = [\"manage_users\", \"manage_groups\", \"delete_account\"]\n
-        WRITE = [\"groups\"]\n
         ' > /opt/canaille/config.toml && \
         export CONFIG=/opt/canaille/config.toml && \
         canaille db upgrade && \
-        canaille create user --user-name admin --password admin --emails admin@mydomain.example --given-name George --family-name Abitbol && \
-        canaille create group --display-name admins --members admin && \
+        canaille create user --user-name admin --password admin && \
+        canaille create group --display-name admin --members admin && \
         canaille run --config /opt/canaille/hypercorn.toml
       "
     ];
