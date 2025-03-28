5670d93af1a813250f7eb39c596f18d83bd2c867
Félix Rohrlich <felix@yaal.coop>
2025-03-28 09:55:16 +0100

    fix: disabled talisman parameter forcing https cookies when testing

--- canaille/app/talisman.py
+++ canaille/app/talisman.py
@@ -2,8 +2,14 @@ from flask_talisman import Talisman
 
 
 def setup_talisman(app):
+    is_https = not app.config["TESTING"]
     csp = {
         "default-src": "'self'",
         "font-src": "'self' data:",
     }
-    Talisman(app, content_security_policy=csp, force_https=not app.config["TESTING"])
+    Talisman(
+        app,
+        content_security_policy=csp,
+        force_https=is_https,
+        session_cookie_secure=is_https,
+    )
