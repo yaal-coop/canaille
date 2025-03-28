c51781471bceee395a1da08367db0fad31afef7a
Éloi Rivard <eloi@yaal.coop>
2025-03-28 19:17:21 +0100

    chore: minor cleanups in canaille.nix

--- canaille.nix
+++ canaille.nix
@@ -25,20 +25,12 @@ pkgs.dockerTools.buildImage {
         pip install canaille[front,oidc,postgresql,server,otp,sms,scim] && \
         mkdir -p /opt/canaille && \
         echo 'bind = [\"0.0.0.0:5000\"]' > /opt/canaille/hypercorn.toml && \
-        echo '
-        SERVER_NAME = \"canaille.localhost:5000\"\n
-        PREFERRED_URL_SCHEME = \"https\"\n
-        [CANAILLE]\n
-        TIMEZONE = \"UTC\"\n
-        [CANAILLE_SQL]\n
-        DATABASE_URI = \"sqlite:///demo.sqlite\"\n
-        ' > /opt/canaille/config.toml && \
-        export CONFIG=/opt/canaille/config.toml && \
-        canaille db upgrade && \
+        canaille install && \
         canaille create user --user-name admin --password admin && \
         canaille create group --display-name admin --members admin && \
         canaille run --config /opt/canaille/hypercorn.toml
       "
     ];
+    ExposedPorts = { "5000/tcp" = { }; };
   };
 }
