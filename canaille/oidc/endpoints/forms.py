b7df0a681c6d404504ae0b6b2e2b568c762b2ead
Éloi Rivard <eloi@yaal.coop>
2025-05-02 15:04:59 +0200

    feat: add descriptions on the client edition/addition form fields

--- canaille/oidc/endpoints/forms.py
+++ canaille/oidc/endpoints/forms.py
@@ -46,24 +46,36 @@ class ClientAddForm(Form):
         _("Name"),
         validators=[wtforms.validators.DataRequired()],
         render_kw={"placeholder": "Client Name"},
+        description=_(
+            "This is the name that will be displayed on the web interface, and notably on the consent page."
+        ),
     )
+
     contacts = wtforms.FieldList(
         wtforms.EmailField(
             _("Contacts"),
             validators=[wtforms.validators.Optional(), email_validator],
-            render_kw={"placeholder": "admin@mydomain.example"},
+            render_kw={"placeholder": "admin@client.example"},
         ),
         min_entries=1,
         validators=[unique_values],
+        description=_(
+            "Those are the email addresses of people responsible for the client."
+        ),
     )
+
     client_uri = wtforms.URLField(
         _("URI"),
         validators=[
             wtforms.validators.DataRequired(),
             is_uri,
         ],
-        render_kw={"placeholder": "https://mydomain.example"},
+        render_kw={"placeholder": "https://client.example"},
+        description=_(
+            "URL string of a web page providing information about the client."
+        ),
     )
+
     redirect_uris = wtforms.FieldList(
         wtforms.URLField(
             _("Redirect URIs"),
@@ -71,10 +83,13 @@ class ClientAddForm(Form):
                 wtforms.validators.DataRequired(),
                 is_uri,
             ],
-            render_kw={"placeholder": "https://mydomain.example/callback"},
+            render_kw={"placeholder": "https://client.example/oauth/callback"},
         ),
         min_entries=1,
         validators=[unique_values],
+        description=_(
+            "URIs for use in redirect-based flows such as the authorization code and implicit flows."
+        ),
     )
     post_logout_redirect_uris = wtforms.FieldList(
         wtforms.URLField(
@@ -83,13 +98,15 @@ class ClientAddForm(Form):
                 wtforms.validators.Optional(),
                 is_uri,
             ],
-            render_kw={
-                "placeholder": "https://mydomain.example/you-have-been-disconnected"
-            },
+            render_kw={"placeholder": "https://client.example/oauth/logout-callback"},
         ),
         min_entries=1,
         validators=[unique_values],
+        description=_(
+            "URIs that the client may redirect users to after logging them out."
+        ),
     )
+
     grant_types = wtforms.SelectMultipleField(
         _("Grant types"),
         validators=[wtforms.validators.DataRequired()],
@@ -103,19 +120,27 @@ class ClientAddForm(Form):
             ("urn:ietf:params:oauth:grant-type:jwt-bearer", "jwt-bearer"),
         ],
         default=["authorization_code", "refresh_token"],
+        description=_("Grant types that the client can use at the token endpoint."),
     )
+
     scope = wtforms.StringField(
         _("Scope"),
         validators=[wtforms.validators.DataRequired()],
         default="openid profile email",
         render_kw={"placeholder": "openid profile"},
+        description=_("Kind of information that the client can request about users."),
     )
+
     response_types = wtforms.SelectMultipleField(
         _("Response types"),
         validators=[wtforms.validators.DataRequired()],
         choices=[("code", "code"), ("token", "token"), ("id_token", "id_token")],
         default=["code"],
+        description=_(
+            "Response types that the client can use at the authorization endpoint."
+        ),
     )
+
     token_endpoint_auth_method = wtforms.SelectField(
         _("Token Endpoint Auth Method"),
         validators=[wtforms.validators.DataRequired()],
@@ -127,65 +152,89 @@ class ClientAddForm(Form):
             ("none", "none"),
         ],
         default="client_secret_basic",
+        description=_(
+            "Authentication method that the client will use at the token endpoint."
+        ),
     )
+
     audience = wtforms.SelectMultipleField(
         _("Token audiences"),
         validators=[wtforms.validators.Optional()],
         choices=_client_audiences,
         validate_choice=False,
         coerce=IDToModel("Client"),
+        description=_(
+            "The other clients that are intended to use the tokens emitted by this client."
+        ),
     )
+
     logo_uri = wtforms.URLField(
         _("Logo URI"),
         validators=[
             wtforms.validators.Optional(),
             is_uri,
         ],
-        render_kw={"placeholder": "https://mydomain.example/logo.png"},
+        render_kw={"placeholder": "https://client.example/logo.png"},
+        description=_("An URL for the logo of this client."),
     )
+
     tos_uri = wtforms.URLField(
         _("Terms of service URI"),
         validators=[
             wtforms.validators.Optional(),
             is_uri,
         ],
-        render_kw={"placeholder": "https://mydomain.example/tos.html"},
+        render_kw={"placeholder": "https://client.example/tos.html"},
+        description=_("URL to the terms of service of the client."),
     )
+
     policy_uri = wtforms.URLField(
         _("Policy URI"),
         validators=[
             wtforms.validators.Optional(),
             is_uri,
         ],
-        render_kw={"placeholder": "https://mydomain.example/policy.html"},
+        render_kw={"placeholder": "https://client.example/policy.html"},
+        description=_("URL to the privacy policy of the client."),
     )
+
     software_id = wtforms.StringField(
         _("Software ID"),
         validators=[wtforms.validators.Optional()],
         render_kw={"placeholder": "xyz"},
+        description=_(
+            "Unique identifier for this client, that should be stable in time and common for all identity providers."
+        ),
     )
+
     software_version = wtforms.StringField(
         _("Software Version"),
         validators=[wtforms.validators.Optional()],
         render_kw={"placeholder": "1.0"},
+        description=_("The version of the client."),
     )
     jwks = wtforms.StringField(
         _("JWKS"),
         validators=[wtforms.validators.Optional(), is_jwks],
         render_kw={"placeholder": ""},
+        description=_("The public JSON Web Keys of the client."),
     )
+
     jwks_uri = wtforms.URLField(
         _("JKWS URI"),
         validators=[
             wtforms.validators.Optional(),
             is_uri,
         ],
-        render_kw={"placeholder": ""},
+        render_kw={"placeholder": "https://client.example/oauth/jwks.json"},
+        description=_("The URI that points to the public JSON Web Keys of the client."),
     )
+
     trusted = wtforms.BooleanField(
         _("Trusted"),
         validators=[wtforms.validators.Optional()],
         default=False,
+        description=_("Whether the clients needs to display consent dialogs."),
     )
 
 
