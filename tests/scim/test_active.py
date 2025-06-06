d49c0057f98bad75a430e6ea1f1e5c321cae67df
Félix Rohrlich <felix@yaal.coop>
2025-06-06 11:13:05 +0200

    test: fix active parameter test

--- tests/scim/test_active.py
+++ tests/scim/test_active.py
@@ -5,16 +5,14 @@ def test_set_active_to_false_locks_user_and_vice_versa(scim_client, scim_user, b
     User = scim_client.get_resource_model("User")
 
     users = scim_client.query(User)
-    distant_scim_user = None
-    if users.resources:
-        distant_scim_user = next(
-            (
-                myuser
-                for myuser in users.resources
-                if myuser.user_name == scim_user.user_name
-            ),
-            None,
-        )
+    distant_scim_user = next(
+        (
+            myuser
+            for myuser in users.resources
+            if myuser.user_name == scim_user.user_name
+        ),
+        None,
+    )
 
     distant_scim_user.active = False
     scim_client.replace(distant_scim_user)
@@ -23,6 +21,12 @@ def test_set_active_to_false_locks_user_and_vice_versa(scim_client, scim_user, b
     assert scim_user.locked
 
     distant_scim_user.active = True
+    scim_client.replace(distant_scim_user)
+    backend.reload(scim_user)
+    assert scim_user.lock_date is None
+    assert not scim_user.locked
+    # "None" active parameter shouldn't change the state
+    distant_scim_user.active = None
     scim_client.replace(distant_scim_user)
     backend.reload(scim_user)
     assert scim_user.lock_date is None
