3bcbd22bc1020aaacbda1c04cb620ac0aa99933b
Éloi Rivard <eloi@yaal.coop>
2025-08-12 15:40:01 +0200

    feat: split SCIM server unit tests

--- tests/scim/test_scim_tester.py
+++ tests/scim/test_scim_tester.py
@@ -1,7 +1,19 @@
+import pytest
 from scim2_tester import Status
 from scim2_tester import check_server
+from scim2_tester.discovery import get_all_available_tags
+from scim2_tester.discovery import get_standard_resource_types
 
 
-def test_scim_tester(scim_client):
-    results = check_server(scim_client, raise_exceptions=True)
-    assert all(result.status == Status.SUCCESS for result in results)
+@pytest.mark.parametrize("tag", get_all_available_tags())
+@pytest.mark.parametrize("resource_type", [None] + get_standard_resource_types())
+def test_individual_filters(scim_client, tag, resource_type):
+    results = check_server(
+        scim_client,
+        raise_exceptions=False,
+        include_tags={tag},
+        resource_types=resource_type,
+    )
+
+    for result in results:
+        assert result.status in (Status.SKIPPED, Status.SUCCESS)
