d0e3538e64538a0bfc6e6e9e5d0f3aa8feeb25aa
Éloi Rivard <eloi@yaal.coop>
2025-09-19 16:48:36 +0200

    chore: create a dedicated hypercorn module

--- canaille/app/commands.py
+++ canaille/app/commands.py
@@ -46,30 +46,6 @@ def install(ctx):
     install(app)
 
 
-if HAS_HYPERCORN:  # pragma: no cover
-
-    @click.command()
-    @with_appcontext
-    @click.option(
-        "--config",
-        default=None,
-        help="Path to a TOML hypercorn configuration file.",
-        type=click.Path(),
-    )
-    def run(config: str | None):
-        """Run Canaille in a hypercorn application server.
-
-        Have a look at the :doc:`Hypercorn configuration documentation <hypercorn:how_to_guides/configuring>` to find how to configure it.
-        """
-        from hypercorn.config import Config
-        from hypercorn.run import run as hypercorn_run
-
-        config_obj = Config.from_toml(config) if config else Config()
-        config_obj.application_path = "canaille.app.server:app"
-        exitcode = hypercorn_run(config_obj)
-        sys.exit(exitcode)
-
-
 @click.group()
 def config():
     """Handle Canaille configuration file."""
@@ -130,6 +106,9 @@ def check():
 
 def register(cli):
     cli.add_command(install)
-    if HAS_HYPERCORN:  # pragma: no branch
-        cli.add_command(run)
     cli.add_command(config)
+
+    if HAS_HYPERCORN:  # pragma: no branch
+        from canaille.hypercorn.commands import run
+
+        cli.add_command(run)
---
+++ canaille/hypercorn/__init__.py
@@ -0,0 +1,1 @@
+# Hypercorn integration module
