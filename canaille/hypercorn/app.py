14129f531b00c24350b43225ff4b10d02a0a5389
Éloi Rivard <eloi@yaal.coop>
2025-09-30 09:36:36 +0200

    fix: Hypercorn launch with proxy settings turned on
    ProxyFixMiddleware was expecting an ASGI app instead of a WSGI app

--- canaille/hypercorn/app.py
+++ canaille/hypercorn/app.py
@@ -1,5 +1,6 @@
 import uuid
 
+from asgiref.wsgi import WsgiToAsgi
 from hypercorn.middleware import ProxyFixMiddleware  # pragma: no cover
 
 from canaille import create_app  # pragma: no cover
@@ -10,6 +11,9 @@ if app.config["SECRET_KEY"] is None:  # pragma: no cover
     app.logger.warning("Missing 'SECRET_KEY' configuration parameter.")
     app.config["SECRET_KEY"] = str(uuid.uuid4())
 
+
 if proxy_mode := app.config["CANAILLE_HYPERCORN"]["PROXY_MODE"]:  # pragma: no cover
     trusted_hops = app.config["CANAILLE_HYPERCORN"]["PROXY_TRUSTED_HOPS"] or 1
-    app = ProxyFixMiddleware(app, mode=proxy_mode, trusted_hops=trusted_hops)
+    app = ProxyFixMiddleware(
+        WsgiToAsgi(app), mode=proxy_mode, trusted_hops=trusted_hops
+    )
