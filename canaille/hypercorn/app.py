6237af1fb72c71a0c2e9d50fbbe928c5833eedcc
Éloi Rivard <eloi@yaal.coop>
2025-11-07 17:07:15 +0100

    refactor: add a create_app method in hypercorn module

--- canaille/hypercorn/app.py
+++ canaille/hypercorn/app.py
@@ -3,17 +3,23 @@ import uuid
 from asgiref.wsgi import WsgiToAsgi
 from hypercorn.middleware import ProxyFixMiddleware  # pragma: no cover
 
-from canaille import create_app  # pragma: no cover
-
-app = create_app(env_file=".env")  # pragma: no cover
-
-if app.config["SECRET_KEY"] is None:  # pragma: no cover
-    app.logger.warning("Missing 'SECRET_KEY' configuration parameter.")
-    app.config["SECRET_KEY"] = str(uuid.uuid4())
+import canaille
 
 
-if proxy_mode := app.config["CANAILLE_HYPERCORN"]["PROXY_MODE"]:  # pragma: no cover
-    trusted_hops = app.config["CANAILLE_HYPERCORN"]["PROXY_TRUSTED_HOPS"] or 1
-    app = ProxyFixMiddleware(
-        WsgiToAsgi(app), mode=proxy_mode, trusted_hops=trusted_hops
-    )
+def create_app(config=None, backend=None, env_file=".env"):  # pragma: no cover
+    app = canaille.create_app(config=config, backend=backend, env_file=env_file)
+
+    if app.config["SECRET_KEY"] is None:
+        app.logger.warning("Missing 'SECRET_KEY' configuration parameter.")
+        app.config["SECRET_KEY"] = str(uuid.uuid4())
+
+    if proxy_mode := app.config["CANAILLE_HYPERCORN"]["PROXY_MODE"]:
+        trusted_hops = app.config["CANAILLE_HYPERCORN"]["PROXY_TRUSTED_HOPS"] or 1
+        app = ProxyFixMiddleware(
+            WsgiToAsgi(app), mode=proxy_mode, trusted_hops=trusted_hops
+        )
+
+    return app
+
+
+app = create_app()
