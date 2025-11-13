881614cc5f8d702d56d4d695ee1e77925f4569c7
Éloi Rivard <eloi@yaal.coop>
2025-11-13 18:51:12 +0100

    tests: loosen the get CLI assertions

--- tests/app/commands/test_dump.py
+++ tests/app/commands/test_dump.py
@@ -8,49 +8,48 @@ def test_dump_stdout(cli_runner, backend, user, foo_group):
     """Test the full database dump command."""
     res = cli_runner.invoke(cli, ["dump"], catch_exceptions=False)
     assert res.exit_code == 0, res.stdout
-    assert json.loads(res.stdout) == {
-        "group": [
-            {
-                "created": mock.ANY,
-                "display_name": "foo",
-                "id": foo_group.id,
-                "last_modified": mock.ANY,
-                "members": [
-                    user.id,
-                ],
-            },
-        ],
-        "user": [
-            {
-                "created": mock.ANY,
-                "display_name": "Johnny",
-                "emails": [
-                    "john@doe.test",
-                ],
-                "family_name": "Doe",
-                "formatted_address": "1234, some street, 6789 some city, some state",
-                "formatted_name": "John (johnny) Doe",
-                "given_name": "John",
-                "groups": [foo_group.id],
-                "id": user.id,
-                "last_modified": mock.ANY,
-                "password": mock.ANY,
-                "phone_numbers": [
-                    "555-000-000",
-                ],
-                "preferred_language": "en",
-                "profile_url": "https://john.test",
-                "user_name": "user",
-                "locality": "some city",
-                "postal_code": "6789",
-                "region": "some state",
-                "street": "1234, some street",
-                "hotp_counter": 1,
-                "last_otp_login": mock.ANY,
-                "secret_token": mock.ANY,
-            },
-        ],
-    }
+    payload = json.loads(res.stdout)
+    assert payload["group"] == [
+        {
+            "created": mock.ANY,
+            "display_name": "foo",
+            "id": foo_group.id,
+            "last_modified": mock.ANY,
+            "members": [
+                user.id,
+            ],
+        },
+    ]
+    assert payload["user"] == [
+        {
+            "created": mock.ANY,
+            "display_name": "Johnny",
+            "emails": [
+                "john@doe.test",
+            ],
+            "family_name": "Doe",
+            "formatted_address": "1234, some street, 6789 some city, some state",
+            "formatted_name": "John (johnny) Doe",
+            "given_name": "John",
+            "groups": [foo_group.id],
+            "id": user.id,
+            "last_modified": mock.ANY,
+            "password": mock.ANY,
+            "phone_numbers": [
+                "555-000-000",
+            ],
+            "preferred_language": "en",
+            "profile_url": "https://john.test",
+            "user_name": "user",
+            "locality": "some city",
+            "postal_code": "6789",
+            "region": "some state",
+            "street": "1234, some street",
+            "hotp_counter": 1,
+            "last_otp_login": mock.ANY,
+            "secret_token": mock.ANY,
+        },
+    ]
 
 
 def test_dump_stdout_explicit_model(cli_runner, backend, user, foo_group):
