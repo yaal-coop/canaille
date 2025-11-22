7790ee471581879b601a09b83b4694687d342324
Éloi Rivard <eloi@yaal.coop>
2025-11-22 13:23:53 +0100

    fix: tooltip positionning compatibility with the CSS zoom property
    https://github.com/fomantic/Fomantic-UI/issues/3407

--- canaille/static/js/base.js
+++ canaille/static/js/base.js
@@ -2,7 +2,6 @@ function onDomChanges() {
     $('.ui.dropdown').each(function(){
         $(this).dropdown({"placeholder": $(this).attr("placeholder")});
     });
-    $('*[title]').popup();
 
     $('.toggle-password-visibility').off('click').on('click', function() {
         togglePasswordVisibility(this);
