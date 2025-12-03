d808def44c5d8f81cc9c502eaaec274916cf1a38
Nicolas Graziano <nicolas.graziano+github@gmail.com>
2025-12-03 15:12:28 +0100

    feat: add toml output format to canaille jwk create command

--- canaille/oidc/commands.py
+++ canaille/oidc/commands.py
@@ -1,7 +1,9 @@
+import enum
 import json
 from datetime import timedelta
 
 import click
+import tomlkit
 from flask import current_app
 from flask.cli import with_appcontext
 from joserfc.jwk import ECKey
@@ -41,37 +43,54 @@ def create():
     """JSON Web Key creation."""
     pass
 
+class OutputFormat(enum.Enum):
+    JSON = enum.auto()
+    TOML = enum.auto()
+
+def _print_key(key, format: OutputFormat):
+    """Print key with the selected output format."""
+    if format == OutputFormat.JSON:
+        click.echo(json.dumps(key.as_dict()))
+    elif format == OutputFormat.TOML:
+        key_as_table = tomlkit.inline_table()
+        key_as_table.update(key.as_dict())
+        click.echo(key_as_table.as_string())
+
 
 @create.command()
 @click.option("--size", default=2048, type=int, help="The key size")
-def rsa(size: int):
+@click.option("--format", type=click.Choice(OutputFormat, case_sensitive=False), default=OutputFormat.JSON, help="Output format")
+def rsa(size: int, format: OutputFormat):
     """Create a RSA JSON Web Key."""
     key = RSAKey.generate_key(size, auto_kid=True)
-    click.echo(json.dumps(key.as_dict()))
+    _print_key(key, format)
 
 
 @create.command()
 @click.option("--size", default=256, type=int, help="The key size")
-def oct(size: int):
+@click.option("--format", type=click.Choice(OutputFormat, case_sensitive=False), default=OutputFormat.JSON, help="Output format")
+def oct(size: int, format: OutputFormat):
     """Create a Oct JSON Web Key."""
     key = OctKey.generate_key(size, auto_kid=True)
-    click.echo(json.dumps(key.as_dict()))
+    _print_key(key, format)
 
 
 @create.command()
 @click.option("--crv", default="P-256", type=str, help="The key CRV")
-def ec(crv: str):
+@click.option("--format", type=click.Choice(OutputFormat, case_sensitive=False), default=OutputFormat.JSON, help="Output format")
+def ec(crv: str, format: OutputFormat):
     """Create a EC JSON Web Key."""
     key = ECKey.generate_key(crv, auto_kid=True)
-    click.echo(json.dumps(key.as_dict()))
+    _print_key(key, format)
 
 
 @create.command()
 @click.option("--crv", default="Ed25519", type=str, help="The key CRV")
-def okp(crv: str):
+@click.option("--format", type=click.Choice(OutputFormat, case_sensitive=False), default=OutputFormat.JSON, help="Output format")
+def okp(crv: str, format: OutputFormat):
     """Create a OKP JSON Web Key."""
     key = OKPKey.generate_key(crv, auto_kid=True)
-    click.echo(json.dumps(key.as_dict()))
+    _print_key(key, format)
 
 
 @click.group()
--- tests/oidc/commands/test_jwk.py
+++ tests/oidc/commands/test_jwk.py
@@ -1,5 +1,6 @@
 import json
 
+import tomlkit
 from joserfc import jwk
 
 from canaille.commands import cli
@@ -11,6 +12,13 @@ def test_create_rsa_key(cli_runner):
     assert res.exit_code == 0, res.stdout
     payload = json.loads(res.stdout)
     jwk.import_key(payload, "RSA")
+
+def test_create_rsa_key_format_toml(cli_runner):
+    """Test the RSA key creation command with output toml."""
+    res = cli_runner.invoke(cli, ["jwk", "create", "rsa", "--format", "toml"], catch_exceptions=False)
+    assert res.exit_code == 0, res.stdout
+    payload = tomlkit.loads("KEY=" + res.stdout).unwrap()
+    jwk.import_key(payload["KEY"], "RSA")
 
 
 def test_create_rsa_key_with_size(cli_runner):
@@ -30,6 +38,12 @@ def test_create_oct_key(cli_runner):
     payload = json.loads(res.stdout)
     jwk.import_key(payload, "oct")
 
+def test_create_oct_key_format_toml(cli_runner):
+    """Test the Oct key creation command with output toml."""
+    res = cli_runner.invoke(cli, ["jwk", "create", "oct", "--format", "toml"], catch_exceptions=False)
+    assert res.exit_code == 0, res.stdout
+    payload = tomlkit.loads("KEY=" + res.stdout).unwrap()
+    jwk.import_key(payload["KEY"], "oct")
 
 def test_create_oct_key_with_size(cli_runner):
     """Test the Oct key creation command with a size parameter."""
@@ -47,6 +61,13 @@ def test_create_ec_key(cli_runner):
     assert res.exit_code == 0, res.stdout
     payload = json.loads(res.stdout)
     jwk.import_key(payload, "EC")
+
+def test_create_ec_key_format_toml(cli_runner):
+    """Test the EC key creation command with output toml."""
+    res = cli_runner.invoke(cli, ["jwk", "create", "ec", "--format", "toml"], catch_exceptions=False)
+    assert res.exit_code == 0, res.stdout
+    payload = tomlkit.loads("KEY=" + res.stdout).unwrap()
+    jwk.import_key(payload["KEY"], "EC")
 
 
 def test_create_ec_key_with_crv(cli_runner):
@@ -66,6 +87,12 @@ def test_create_opk_key(cli_runner):
     payload = json.loads(res.stdout)
     jwk.import_key(payload, "OKP")
 
+def test_create_okp_key_format_toml(cli_runner):
+    """Test the OKP key creation command with output toml."""
+    res = cli_runner.invoke(cli, ["jwk", "create", "okp", "--format", "toml"], catch_exceptions=False)
+    assert res.exit_code == 0, res.stdout
+    payload = tomlkit.loads("KEY=" + res.stdout).unwrap()
+    jwk.import_key(payload["KEY"], "OKP")
 
 def test_create_opk_key_with_crv(cli_runner):
     """Test the OPK key creation command with a crv param."""
