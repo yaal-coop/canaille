5ccd5fe2f0555ceb7264f59b9545ec4cc54f7986
Éloi Rivard <eloi@yaal.coop>
2026-01-14 16:46:57 +0100

    fix: Fomantic CSP issues
    There wase JS fix script, but it must be loaded before semantic.min.js

--- canaille/static/js/base.js
+++ canaille/static/js/base.js
@@ -52,27 +52,3 @@ document.body.addEventListener('htmx:beforeOnLoad', function (evt) {
         evt.detail.isError = false;
     }
 });
-
-// the following code's purpose is to make jquery compatible with a strict content security policy
-// https://github.com/fomantic/Fomantic-UI/issues/214#issuecomment-1002927066
-
-var setAttribute_ = Element.prototype.setAttribute;
-
-Element.prototype.setAttribute = function (attr, val) {
-    if (attr.toLowerCase() !== 'style') {
-        setAttribute_.apply(this, [attr, val]);
-    } else {
-        var arr = val.split(';').map( (el, index) => el.trim() );
-        for (var i=0, tmp; i < arr.length; ++i) {
-            if (! /:/.test(arr[i]) ) continue;
-            tmp = arr[i].split(':').map( (el, index) => el.trim() );
-            this.style[ camelize(tmp[0]) ] = tmp[1];
-        }
-    }
-}
-
-function camelize(str) {
-    return str.split('-').map(
-        (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
-    ).join('');
-}
